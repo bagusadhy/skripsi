c84894d5c5f06cc5985268553b3b0dbefdb295a7
Bagusadhy_ <bagusadhy182@gmail.com>
2023-06-21 00:12:48 +0700

    tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,30 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+
+// const withMT = require("@material-tailwind/html/utils/withMT");
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './vendor/laravel/jetstream/**/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/**/*.js",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                primary: "#2F55D4",
+                primaryhover: "#294ab6",
+                secondary: "#F1A73B",
+                secondaryhover: "#c68a2f",
+                success: "#3B8439",
             },
         },
     },
