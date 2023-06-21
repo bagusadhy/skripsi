1dc84cd1f29b246a5338258f105af24245dd870a
Bagusadhy_ <bagusadhy182@gmail.com>
2023-06-22 02:32:43 +0700

    add dashboard page

---
+++ resources/views/pages/backsite/dashboard/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <main class="basis-9/12 bg-white min-h-screen px-10 py-5 ">
+    </main>
+    </div>
+    </section>
+@endsection
