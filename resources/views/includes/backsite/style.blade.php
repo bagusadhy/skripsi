873b7f22346de5a1b43b38927f18eb47c8166f36
Bagusadhy_ <bagusadhy182@gmail.com>
2023-06-22 02:31:56 +0700

    add include

---
+++ resources/views/includes/backsite/style.blade.php
@@ -0,0 +1,5 @@
+<link rel="preconnect" href="{{ url('https://fonts.googleapis.com') }}">
+<link rel="preconnect" href="{{ url('https://fonts.gstatic.com') }}" crossorigin>
+<link href="{{ url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins&display=swap') }}"
+    rel="stylesheet">
+@vite('resources/css/app.css')
