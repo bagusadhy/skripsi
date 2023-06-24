474a1004be8e159edc38f5de512099a571074c33
Bagusadhy_ <bagusadhy182@gmail.com>
2023-06-24 09:23:24 +0700

    done bidang usaha modul

--- app/Http/Controllers/Backsite/BidangUsahaController.php
+++ app/Http/Controllers/Backsite/BidangUsahaController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Backsite;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\MasterData\BidangUsaha;
+
+use App\Http\Requests\BidangUsaha\StoreBidangUsahaRequest;
+use App\Http\Requests\BidangUsaha\UpdateBidangUsahaRequest;
 
 class BidangUsahaController extends Controller
 {
@@ -17,7 +20,8 @@ class BidangUsahaController extends Controller
      */
     public function index()
     {
-        return view('pages.backsite.master-data.bidang-usaha.index');
+        $data = BidangUsaha::orderBy('id', 'ASC')->get();
+        return view('pages.backsite.master-data.bidang-usaha.index', compact('data'));
     }
 
 
@@ -26,15 +30,19 @@ class BidangUsahaController extends Controller
      */
     public function create()
     {
-        //
+        return abort(404);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreBidangUsahaRequest $request)
     {
-        //
+        $data = $request->all();
+        BidangUsaha::create($data);
+
+        alert()->success('Success Message', 'Berhasil Menambahkan Data');
+        return redirect(route('backsite.bidang_usaha.index'));
     }
 
     /**
@@ -42,30 +50,37 @@ class BidangUsahaController extends Controller
      */
     public function show(string $id)
     {
-        //
+        return abort(404);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(BidangUsaha $bidangUsaha)
     {
-        //
+        return view('pages.backsite.master-data.bidang-usaha.edit', compact('bidangUsaha'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateBidangUsahaRequest $request, BidangUsaha $bidangUsaha)
     {
-        //
+        $data = $request->all();
+        $bidangUsaha->update($data);
+
+        alert()->success('Success Message', 'Berhasil Mengubah Data');
+        return redirect(route('backsite.bidang_usaha.index'));
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(BidangUsaha $bidangUsaha)
     {
-        //
+        $bidangUsaha->forceDelete();
+
+        alert()->success('Success Message', 'Berhasil Menghapus Data');
+        return back();
     }
 }
