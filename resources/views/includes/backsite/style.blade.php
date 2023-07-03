af7db88fc292d9ca86cc9b71aabf6bea948c33c0
Bagusadhy_ <bagus.adhy182@gmail.com>
2023-07-04 01:27:03 +0700

    add custom styles for datatables

--- resources/views/includes/backsite/style.blade.php
+++ resources/views/includes/backsite/style.blade.php
@@ -4,7 +4,40 @@
 
 <link rel="preconnect" href="{{ url('https://fonts.googleapis.com') }}">
 <link rel="preconnect" href="{{ url('https://fonts.gstatic.com') }}" crossorigin>
-<link
-    href="{{ url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins&display=swap') }}"
+<link href="{{ url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins&display=swap') }}"
     rel="stylesheet">
 @vite('resources/css/app.css')
+
+<style>
+    .dataTables_wrapper {
+        padding: .1rem !important;
+    }
+
+    .dataTables_length select {
+        width: 60px;
+        background-position: left 1.8rem center !important;
+        border-radius: 5px !important;
+    }
+
+    .dataTables_length:focus select:focus {
+        border: none !important;
+    }
+
+    .dataTables_length,
+    .dataTables_filter {
+        margin-bottom: 2.5rem !important;
+    }
+
+    .dataTables_filter input {
+        border-radius: 5px !important;
+    }
+
+    .dataTables_wrapper:focus .dataTables_filter input:focus {
+        border: none !important;
+    }
+
+    .dataTables_wrapper .dataTables_paginate .paginate_button.current {
+        background: none !important;
+        border: none !important;
+    }
+</style>
--- resources/views/includes/frontsite/style.blade.php
+++ resources/views/includes/frontsite/style.blade.php
@@ -13,3 +13,36 @@
     href="{{ url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Poppins&display=swap') }}"
     rel="stylesheet">
 @vite('resources/css/app.css')
+
+<style>
+
+    .dataTables_wrapper{
+        padding: .1rem !important;
+    }
+     .dataTables_length select {
+        width: 60px;
+        background-position: left 1.8rem center !important;
+        border-radius: 5px !important;
+    }
+     .dataTables_length:focus select:focus {
+        border: none !important;
+    }
+
+    .dataTables_length,
+    .dataTables_filter {
+        margin-bottom: 2.5rem !important;
+    }
+
+    .dataTables_filter input {
+        border-radius: 5px !important;
+    }
+    .dataTables_wrapper:focus .dataTables_filter input:focus {
+        border: none !important;
+    }
+
+    .dataTables_wrapper .dataTables_paginate .paginate_button.current{
+        background: none !important;
+        border: none !important;
+    }
+
+</style>
