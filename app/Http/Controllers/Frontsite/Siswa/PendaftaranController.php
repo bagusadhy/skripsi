365fdc87178cce7577711d0118e8b2905e3df37a
Bagusadhy_ <bagus.adhy182@gmail.com>
2023-07-19 12:36:33 +0700

    change index & store function

--- app/Http/Controllers/Frontsite/Siswa/PendaftaranController.php
+++ app/Http/Controllers/Frontsite/Siswa/PendaftaranController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\Frontsite\Siswa;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 use App\Models\MasterData\Mitra;
 use App\Models\MasterData\Siswa;
 use App\Models\Kegiatan\PendaftarPkl;
+use App\Models\Kegiatan\Lowongan;
+use App\Models\Kegiatan\PesertaPkl;
+use App\Models\MasterData\PeriodePkl;
+use App\Models\Kegiatan\Bimbingan;
 
 use App\Http\Requests\PendaftarPkl\StorePendaftarPklRequest;
 
@@ -20,11 +26,13 @@ class PendaftaranController extends Controller
 
         $siswa = Siswa::where('user_id', auth()->user()->id)->first();
 
-        $pendaftaran = PendaftarPkl::select('mitra_id')->where('siswa_id', $siswa->id)->get()->toArray();
-        $mitra = Mitra::whereNotIn('id', $pendaftaran)->with('bidang_usaha')->get();
-        $mitra_terdaftar = PendaftarPkl::where('siswa_id', $siswa->id)->with('mitra', 'mitra.bidang_usaha')->get();
+        $pendaftaran = PendaftarPkl::select('lowongan_id')->where('siswa_id', $siswa->id)->get()->toArray();
+        $lowongan = Lowongan::whereNotIn('id', $pendaftaran)->with('mitra', 'mitra.bidang_usaha')->get();
+        $lowongan_terdaftar = PendaftarPkl::where('siswa_id', $siswa->id)->with('lowongan', 'mitra', 'mitra.bidang_usaha')->get();
 
-        return view('pages.frontsite.siswa.mitra', compact('mitra', 'mitra_terdaftar'));
+        $peserta = PesertaPkl::where('siswa_id', $siswa->id)->first();
+
+        return view('pages.frontsite.siswa.mitra', compact('lowongan', 'lowongan_terdaftar', 'peserta'));
     }
 
     /**
@@ -68,9 +76,29 @@ class PendaftaranController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, PendaftarPkl $pendaftaran)
     {
-        //
+
+        DB::transaction(function () use ($pendaftaran){
+            PendaftarPkl::where('siswa_id', $pendaftaran->siswa_id)->update(['status' => '2']);
+            $pendaftaran->update(['status' => '3']);
+
+            $periode = PeriodePkl::where('status', '1')->first();
+            $guru = Bimbingan::where('mitra_id', $pendaftaran->mitra_id)->first();
+
+            $peserta = [
+                'siswa_id' => $pendaftaran->siswa_id,
+                'mitra_id' => $pendaftaran->mitra_id,
+                'lowongan_id' => $pendaftaran->lowongan_id,
+                'guru_id' => $guru->guru_id,
+                'periode_id' => $periode->id
+            ];
+
+            PesertaPkl::create($peserta);
+            alert()->success('Berhasil', 'Pendaftaran Diterima');
+        });
+
+        return back();
     }
 
     /**
