3e0cab99727d937f90b682bc4a1f1aaf1fb0cff9
bagusadhy <bagus.adhy182@gmail.com>
2023-08-03 09:41:37 +0700

    add dashboard modul

--- app/Http/Controllers/Backsite/DashboardController.php
+++ app/Http/Controllers/Backsite/DashboardController.php
@@ -5,6 +5,13 @@ namespace App\Http\Controllers\Backsite;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\MasterData\Siswa;
+use App\Models\MasterData\Guru;
+use App\Models\MasterData\Mitra;
+use App\Models\Kegiatan\PengajuanMitra;
+use App\Models\Kegiatan\Lowongan;
+use App\Models\Kegiatan\AktivitasSiswa;
+
 class DashboardController extends Controller
 {
 
@@ -19,7 +26,24 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('pages.backsite.dashboard.index');
+        $siswa = Siswa::select(Siswa::raw('COUNT(id) as total_siswa'))->first();
+        $guru = Guru::select(Guru::raw('COUNT(id) as total_guru'))->first();
+        $mitra = Mitra::select(Mitra::raw('COUNT(id) as total_mitra'))->first();
+        $pengajuan_mitra = PengajuanMitra::select(PengajuanMitra::raw('COUNT(id) as total_pengajuan_mitra'))->first();
+        $lowongan = Lowongan::select(Lowongan::raw('COUNT(id) as total_lowongan'))->first();
+
+        $tanggal = date('Y-m-d');
+
+        $presensi = AktivitasSiswa::select(AktivitasSiswa::raw('COUNT(CASE WHEN presensi = 1 THEN 1 ELSE NULL END) as hadir, COUNT(CASE WHEN presensi = 2 THEN 1 ELSE NULL END) as izin, COUNT(CASE WHEN presensi = 3 THEN 1 ELSE NULL END) as sakit, COUNT(CASE WHEN presensi = 4 THEN 1 ELSE NULL END) as libur'))->where('tanggal', $tanggal)->groupBy('presensi')->get()->toArray();
+
+        $dataset_presensi = [];
+        if($presensi == null){
+            $dataset_presensi = json_encode([0, 0, 0, 0]);
+        }else{
+            $dataset_presensi = json_encode(array_values($presensi[0]));
+        }
+
+        return view('pages.backsite.dashboard.index', compact('siswa', 'guru', 'mitra', 'pengajuan_mitra', 'lowongan', 'dataset_presensi'));
     }
 
     /**
