7ecd135fccedf6d9c60b1a97fa31a210f1748be9
bagusadhy <bagus.adhy182@gmail.com>
2023-08-11 00:26:28 +0700

    add dashboard mitra

--- app/Http/Controllers/Frontsite/Mitra/DashboardController.php
+++ app/Http/Controllers/Frontsite/Mitra/DashboardController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers\Frontsite\Mitra;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kegiatan\PendaftarPkl;
 use Illuminate\Http\Request;
+
+use App\Models\Kegiatan\PesertaPkl;
+use App\Models\Kegiatan\Lowongan;
+use App\Models\MasterData\Mitra;
+use App\Models\Kegiatan\AktivitasSiswa;
 
 class DashboardController extends Controller
 {
@@ -12,7 +18,22 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('pages.frontsite.mitra.index');
+        $mitra_id = Mitra::where('user_id', auth()->user()->id)->first()->id;
+        $siswa = PesertaPkl::select(PesertaPkl::raw('COUNT(id) as total_siswa'))->where('mitra_id', $mitra_id)->first();
+        $lowongan = Lowongan::select(Lowongan::raw('COUNT(id) as total_lowongan'))->where('mitra_id', $mitra_id)->first();
+        $pendaftar = PendaftarPkl::select(PendaftarPkl::raw('COUNT(id) as total_pendaftar'))->where('mitra_id', $mitra_id)->first();
+        $peserta = PesertaPkl::select('siswa_id')->where('mitra_id', $mitra_id)->get()->toArray();
+
+        $presensi = AktivitasSiswa::select(AktivitasSiswa::raw('COUNT(CASE WHEN presensi = 1 THEN 1 ELSE NULL END) as hadir, COUNT(CASE WHEN presensi = 2 THEN 1 ELSE NULL END) as izin, COUNT(CASE WHEN presensi = 3 THEN 1 ELSE NULL END) as sakit, COUNT(CASE WHEN presensi = 4 THEN 1 ELSE NULL END) as libur'))->where('tanggal', date('Y-m-d'))->whereIn('siswa_id', $peserta)->groupBy('presensi')->get()->toArray();
+
+        $dataset_presensi = [];
+        if ($presensi == null) {
+            $dataset_presensi = json_encode([0, 0, 0, 0]);
+        } else {
+            $dataset_presensi = json_encode(array_values($presensi[0]));
+        }
+
+        return view('pages.frontsite.mitra.index', compact('siswa', 'pendaftar', 'lowongan','dataset_presensi'));
     }
 
     /**
